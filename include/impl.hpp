1c185650ce9e4da2eaafdd367c3d88c0655ba2e6
Peter Züger <zueger.peter@icloud.com>
2019-02-27 19:58:00 +0100

    added sha256 implementation based on the implementation from Brad Conte (brad AT bradconte.com)

--- include/impl.hpp
+++ include/impl.hpp
@@ -9,12 +9,42 @@
 #define LIBCRYPT_IMPL_HPP
 
 #include <cstdint>
+#include <cstddef>
 
 namespace libcrypt{
     namespace impl{
         template<typename T>
         constexpr T ROTLEFT(T a, std::size_t b){
             return ((a << b) | (a >> (32 - b)));
+        }
+
+        template<typename T>
+        constexpr T ROTRIGHT(T a, std::size_t b){
+            return (((a) >> (b)) | ((a) << (32-(b))));
+        }
+
+        constexpr std::uint32_t CH(std::uint32_t x, std::uint32_t y, std::uint32_t z){
+            return (((x) & (y)) ^ (~(x) & (z)));
+        }
+
+        constexpr std::uint32_t MAJ(std::uint32_t x, std::uint32_t y, std::uint32_t z){
+            return (((x) & (y)) ^ ((x) & (z)) ^ ((y) & (z)));
+        }
+
+        constexpr std::uint32_t EP0(std::uint32_t x){
+            return (ROTRIGHT(x,2) ^ ROTRIGHT(x,13) ^ ROTRIGHT(x,22));
+        }
+
+        constexpr std::uint32_t EP1(std::uint32_t x){
+            return (ROTRIGHT(x,6) ^ ROTRIGHT(x,11) ^ ROTRIGHT(x,25));
+        }
+
+        constexpr std::uint32_t SIG0(std::uint32_t x){
+            return (ROTRIGHT(x,7) ^ ROTRIGHT(x,18) ^ ((x) >> 3));
+        }
+
+        constexpr std::uint32_t SIG1(std::uint32_t x){
+            return (ROTRIGHT(x,17) ^ ROTRIGHT(x,19) ^ ((x) >> 10));
         }
     }
 }
---
+++ include/sha256.hpp
@@ -0,0 +1,149 @@
+/**
+ * @file   sha256.hpp
+ * @author Brad Conte (brad AT bradconte.com)
+ * @author Peter Zueger
+ * @date   27.02.2019
+ * @brief  sha256 hash implementation
+ */
+#ifndef SHA256_HPP
+#define SHA256_HPP
+
+#include <cstdint>
+#include <array>
+#include "impl.hpp"
+
+namespace libcrypt{
+    class sha256{
+        std::array<std::uint8_t, 64> data;
+        std::uint32_t datalen;
+        std::uint64_t bitlen;
+        std::array<std::uint32_t, 8> state;
+        constexpr static std::array<std::uint32_t, 64> k{
+            0x428a2f98,0x71374491,0xb5c0fbcf,0xe9b5dba5,0x3956c25b,0x59f111f1,0x923f82a4,0xab1c5ed5,
+            0xd807aa98,0x12835b01,0x243185be,0x550c7dc3,0x72be5d74,0x80deb1fe,0x9bdc06a7,0xc19bf174,
+            0xe49b69c1,0xefbe4786,0x0fc19dc6,0x240ca1cc,0x2de92c6f,0x4a7484aa,0x5cb0a9dc,0x76f988da,
+            0x983e5152,0xa831c66d,0xb00327c8,0xbf597fc7,0xc6e00bf3,0xd5a79147,0x06ca6351,0x14292967,
+            0x27b70a85,0x2e1b2138,0x4d2c6dfc,0x53380d13,0x650a7354,0x766a0abb,0x81c2c92e,0x92722c85,
+            0xa2bfe8a1,0xa81a664b,0xc24b8b70,0xc76c51a3,0xd192e819,0xd6990624,0xf40e3585,0x106aa070,
+            0x19a4c116,0x1e376c08,0x2748774c,0x34b0bcb5,0x391c0cb3,0x4ed8aa4a,0x5b9cca4f,0x682e6ff3,
+            0x748f82ee,0x78a5636f,0x84c87814,0x8cc70208,0x90befffa,0xa4506ceb,0xbef9a3f7,0xc67178f2
+        };
+
+        void transform(){
+            using namespace impl;
+            std::array<std::uint32_t, 64> m;
+            std::uint32_t a, b, c, d, e, f, g, h, i, j, t1, t2;
+
+            for(i = 0, j = 0; i < 16; ++i, j += 4)
+                m[i] = (data[j] << 24) | (data[j + 1] << 16) | (data[j + 2] << 8) | (data[j + 3]);
+            for( ; i < 64; ++i)
+                m[i] = SIG1(m[i - 2]) + m[i - 7] + SIG0(m[i - 15]) + m[i - 16];
+
+            a = state[0];
+            b = state[1];
+            c = state[2];
+            d = state[3];
+            e = state[4];
+            f = state[5];
+            g = state[6];
+            h = state[7];
+
+            for (i = 0; i < 64; ++i) {
+                t1 = h + EP1(e) + CH(e,f,g) + k[i] + m[i];
+                t2 = EP0(a) + MAJ(a,b,c);
+                h = g;
+                g = f;
+                f = e;
+                e = d + t1;
+                d = c;
+                c = b;
+                b = a;
+                a = t1 + t2;
+            }
+
+            state[0] += a;
+            state[1] += b;
+            state[2] += c;
+            state[3] += d;
+            state[4] += e;
+            state[5] += f;
+            state[6] += g;
+            state[7] += h;
+        }
+
+    public:
+        sha256(){
+            datalen = 0;
+            bitlen = 0;
+            state[0] = 0x6a09e667;
+            state[1] = 0xbb67ae85;
+            state[2] = 0x3c6ef372;
+            state[3] = 0xa54ff53a;
+            state[4] = 0x510e527f;
+            state[5] = 0x9b05688c;
+            state[6] = 0x1f83d9ab;
+            state[7] = 0x5be0cd19;
+        }
+
+        template<typename T>
+        void update(const T& _data){
+            for(const auto& i : _data){
+                data[datalen] = i;
+                datalen++;
+                if(datalen == 64){
+                    transform();
+                    bitlen += 512;
+                    datalen = 0;
+                }
+            }
+        }
+
+        std::array<std::uint8_t,32> final(){
+            std::array<std::uint8_t,32> hash;
+            std::uint32_t i = datalen;
+
+            // Pad whatever data is left in the buffer.
+            if(datalen < 56){
+                data[i++] = 0x80;
+                while(i < 56)
+                    data[i++] = 0x00;
+            }else{
+                data[i++] = 0x80;
+                while(i < 64)
+                    data[i++] = 0x00;
+                transform();
+                for(std::size_t i = 0; i < 56; i++)
+                    data[i] = 0;
+            }
+
+            // Append to the padding the total message's length in bits and transform.
+            bitlen += datalen * 8;
+            data[63] = static_cast<std::uint8_t>(bitlen);
+            data[62] = static_cast<std::uint8_t>(bitlen >> 8);
+            data[61] = static_cast<std::uint8_t>(bitlen >> 16);
+            data[60] = static_cast<std::uint8_t>(bitlen >> 24);
+            data[59] = static_cast<std::uint8_t>(bitlen >> 32);
+            data[58] = static_cast<std::uint8_t>(bitlen >> 40);
+            data[57] = static_cast<std::uint8_t>(bitlen >> 48);
+            data[56] = static_cast<std::uint8_t>(bitlen >> 56);
+            transform();
+
+            // Since this implementation uses little endian byte ordering and SHA uses big endian,
+            // reverse all the bytes when copying the final state to the output hash.
+            for(i = 0; i < 4; ++i){
+                hash[i]      = (state[0] >> (24 - i * 8)) & 0x000000ff;
+                hash[i + 4]  = (state[1] >> (24 - i * 8)) & 0x000000ff;
+                hash[i + 8]  = (state[2] >> (24 - i * 8)) & 0x000000ff;
+                hash[i + 12] = (state[3] >> (24 - i * 8)) & 0x000000ff;
+                hash[i + 16] = (state[4] >> (24 - i * 8)) & 0x000000ff;
+                hash[i + 20] = (state[5] >> (24 - i * 8)) & 0x000000ff;
+                hash[i + 24] = (state[6] >> (24 - i * 8)) & 0x000000ff;
+                hash[i + 28] = (state[7] >> (24 - i * 8)) & 0x000000ff;
+            }
+
+            return hash;
+        }
+    };
+}
+
+#endif /* SHA256_HPP */
