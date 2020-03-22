57e57138007667ed2a3c57d121fe2ca821fcbcc7
Peter Züger <zueger.peter@icloud.com>
2020-03-22 10:53:03 +0100

    added sdbm hash implementation

---
+++ include/sdbm.hpp
@@ -0,0 +1,66 @@
+/**
+ * @file   libcrypt/include/sdbm.hpp
+ * @author Peter Züger
+ * @date   20.03.2020
+ * @brief  sdbm hash function
+ *
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2020 Peter Züger
+ *
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use, copy,
+ * modify, merge, publish, distribute, sublicense, and/or sell copies
+ * of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+ * BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+ * ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+#ifndef LIBCRYPT_SDBM_HPP
+#define LIBCRYPT_SDBM_HPP
+
+#include <cstdint>
+#include <iterator>
+
+namespace crypt{
+    class sdbm{
+        std::uint32_t hash;
+
+    public:
+        sdbm(): hash{0}{}
+
+        template<typename T>
+        void update(const T& byte){
+            static_assert((sizeof(T) == 1),
+                          "crypt::djb2::update: T must be byte");
+            hash = byte + (hash << 6) + (hash << 16) - hash;
+        }
+
+        template<typename Iterator>
+        void update(Iterator first, Iterator last){
+            static_assert((sizeof(typename std::iterator_traits<Iterator>::value_type) == 1),
+                          "crypt::djb::update: T::value_type must be byte");
+            for(; first != last; ++first){
+                update(*first);
+            }
+        }
+
+        std::uint32_t final(){
+            return hash;
+        }
+    };
+}
+
+#endif /* LIBCRYPT_SDBM_HPP */
